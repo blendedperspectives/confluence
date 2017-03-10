e092e194e4c7aaa03de4e9615becec8eb4f3841d
Patrick Connolly <patrick.c.connolly@gmail.com>
2017-03-09 22:17:54 -0500

    Simpler docker test.

--- .delivery/build_cookbook/recipes/unit.rb
+++ .delivery/build_cookbook/recipes/unit.rb
@@ -3,7 +3,6 @@
 # Recipe:: unit
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
-include_recipe 'delivery-truck::unit'
 
 # Test-converge on our most common platform
 delivery_test_kitchen 'quality_converge_destroy_ubuntu_1404' do
@@ -11,7 +10,9 @@ delivery_test_kitchen 'quality_converge_destroy_ubuntu_1404' do
   # Avoid `--parallel` just in case
   options '--concurrency=4'
   yaml '.kitchen.docker.yml'
-  suite 'ubuntu-1404'
+  #suite 'ubuntu-1404'
+  suite 'standalone-postgresql-ubuntu-1404'
   action [:converge, :destroy]
 end
 
+include_recipe 'delivery-truck::unit'
