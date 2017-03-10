1428e3b713475246192fb798ca70fd4c95104fc1
Patrick Connolly <patrick.c.connolly@gmail.com>
2017-03-10 01:36:19 -0500

    Trying a different approach to merging kitchen files.

--- .delivery/build_cookbook/recipes/unit.rb
+++ .delivery/build_cookbook/recipes/unit.rb
@@ -4,15 +4,11 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-link "#{delivery_workspace_repo}/.kitchen.local.yml" do
-  to "#{delivery_workspace_repo}/.kitchen.ec2.yml"
-end
-
 delivery_test_kitchen 'unit_create' do
   driver 'ec2'
   options '--log-level=debug'
   suite 'standalone-postgresql-ubuntu-1404'
-  repo_path delivery_workspace_repo
+  repo_path "#{delivery_workspace_repo}/.delivery/.kitchen.ec2.yml"
   action :create
 end
 
