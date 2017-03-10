be1384a5b3619f839bcac094ad8190a241a77c45
Patrick Connolly <patrick.c.connolly@gmail.com>
2017-03-09 19:42:52 -0500

    Moved test-kitchen debugging from unit to quality.

--- .delivery/build_cookbook/recipes/unit.rb
+++ .delivery/build_cookbook/recipes/unit.rb
@@ -3,12 +3,4 @@
 # Recipe:: unit
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
-
-delivery_test_kitchen 'unit_create' do
-  driver 'dokken'
-  yaml '.kitchen.docker.yml'
-  suite 'standalone-postgresql-centos-7'
-  action [:converge, :destroy]
-end
-
 include_recipe 'delivery-truck::unit'
